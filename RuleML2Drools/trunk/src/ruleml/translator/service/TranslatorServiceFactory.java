819eacab64573832f185437906afb343d0c3ee3f
tisho_jaba <tisho_jaba@85dc8ae1-822f-0410-b2e9-8688500e249e>
2011-10-01 18:14:52 +0000

    ServiceLocator pluggin enabled

--- RuleML2Drools/trunk/src/ruleml/translator/service/TranslatorServiceFactory.java
+++ RuleML2Drools/trunk/src/ruleml/translator/service/TranslatorServiceFactory.java
@@ -1,34 +1,41 @@
 package ruleml.translator.service;
 
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
 import ruleml.translator.drl2ruleml.Drools2RuleMLTranslator;
 import ruleml.translator.prova2ruleml.Prova2RuleMLTranslator;
 import ruleml.translator.ruleml2drl.RuleML2DroolsTranslator;
 import ruleml.translator.ruleml2prova.RuleML2ProvaTranslator;
 
 /**
- * Creates a translator servicea and makes the configuraiton for it. It couples the instances 
- * of the individual translators with the central translator service.
+ * Creates a translator servicea and makes the configuraiton for it. It couples
+ * the instances of the individual translators with the central translator
+ * service.
  * 
  * @author jabarski
  */
 public class TranslatorServiceFactory {
-	public static RulesTranslatorService createTranslatorService () {
+	public static RulesTranslatorService createTranslatorService() {
 		RulesTranslatorServiceImpl translatorService = new RulesTranslatorServiceImpl();
+
 		try {
-			translatorService.addTranslator(new Drools2RuleMLTranslator());
-			
-			translatorService.addTranslator(new RuleML2DroolsTranslator());
-			
-			translatorService.addTranslator(new Prova2RuleMLTranslator());
-			
-			RuleML2ProvaTranslator ruleml2ProvaTranslator = new RuleML2ProvaTranslator();
-			ruleml2ProvaTranslator.setXSLT("resources/rrml2prova_1.0.xsl");
-			translatorService.addTranslator(ruleml2ProvaTranslator);
+			ServiceLoader<Translator> translatorsLoader = ServiceLoader
+					.load(Translator.class);
+			Iterator<Translator> iterator = translatorsLoader.iterator();
+			while (iterator.hasNext()) {
+				Translator translator = (Translator) iterator.next();
+				translatorService.addTranslator(translator);
+			}
+
+//			RuleML2ProvaTranslator ruleml2ProvaTranslator = new RuleML2ProvaTranslator();
+//			ruleml2ProvaTranslator.setXSLT("resources/rrml2prova_1.0.xsl");
+//			translatorService.addTranslator(ruleml2ProvaTranslator);
 
 		} catch (UnknownRulesLanguageException e) {
 			e.printStackTrace();
 		}
-		
+
 		return translatorService;
 	}
 }
